877d92ca51ba5a0c4d6a01a6a5d8042975a81b55
RehanFargose <rehanfargose@gmail.com>
2025-04-12 11:36:31 +0530

    Added Delete functionality

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -33,8 +33,13 @@ export default function App() {
 
   // useEffect hook to retrieve and display all Todos during 1st load and page refresh
   useEffect(()=>{
+    // Code that does some work, when something in dependency array tells it to
     // Call the function to display list in the beggining
     getTodos();
+
+    // Dependency array, tells what useEffect should react to and perform the above code
+    // Even if the dep array is empty, useEffect always runs at least once when component mounts
+    // Usually provide useState const in dep array
   }, []);
 
 
@@ -60,7 +65,20 @@ export default function App() {
 
   // function to get id and delete a todo item from db
   async function delTodo(todoID) {
-    
+    try {
+      console.log("Trying to delete todo with db id as: "+todoID);
+      
+      const delResponse = await axios.delete(API_URL+"/delete/"+todoID);
+      console.log(delResponse.data);
+      
+      
+      // api endpt to get all posts stored in db
+      // const getresp = await getTodos();
+      getTodos();
+
+    } catch (error) {
+      console.error(error.message);
+    }
   }
 
 
@@ -78,7 +96,9 @@ export default function App() {
 
     {/* Display all the todos */}
     {/* Use elem.todo_id which is PK in DB to eventually create delete and edit functionality */}
-    {todos.map((elem, index)=>(<ListTodos key={index} id={elem.todo_id} title={elem.title} content={elem.content}/>))}
+    {todos.map((elem, index)=>(
+      <ListTodos key={index} id={elem.todo_id} title={elem.title} content={elem.content} delFunc={delTodo}/>
+      ))}
 
   </div>
   )
--- client/src/components/listTodos.jsx
+++ client/src/components/listTodos.jsx
@@ -8,7 +8,14 @@ export default function ListTodos(props){
     return (<div>
             <h3>{props.id} {props.title}</h3>
             <p>{props.content}</p>
-            <button className="btn btn-danger">Delete</button>
+            <button className="btn btn-danger" onClick={(event)=>{
+                event.preventDefault();
+
+                // Call the delete function and pass the props.id which is a db PK
+                // to the delete function passed here as a prop
+                props.delFunc(props.id);
+
+            }}>Delete</button>
             <button className="btn btn-primary">Edit</button>
     </div>)
 }
